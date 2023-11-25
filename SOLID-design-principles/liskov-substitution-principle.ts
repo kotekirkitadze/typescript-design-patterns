c873c4ae0fe5ef592cd2cb577bcf009e285d6794
Konstantine Kirkitadze <74476606+kotekirkitadze@users.noreply.github.com>
2023-11-25 22:33:04 +0400

    add liskob subsitution pricnciple example

--- SOLID-design-principles/liskov-substitution-principle.ts
+++ SOLID-design-principles/liskov-substitution-principle.ts
@@ -38,3 +38,85 @@ class Penguin extends FlightlessBird {}
 
 const penguin = new Penguin();
 penguin.fly();
+
+
+//Example two
+
+abstract class Shape{
+	abstract calculateArea(): number;
+}
+
+class Rectangle extends Shape{
+	constructor(public width: number, public height: number){
+		super();
+	}
+	calculateArea(): number{
+		return this.width * this.height;
+	}
+}
+
+class Square extends Shape{
+	constructor(public side: number){
+		super();
+	}
+	calculateArea(): number{
+		return this.side * this.side;
+	}
+}
+
+//client code
+
+function calculateArea(shape: Shape): number{
+	return shape.calculateArea();
+}
+
+const rectangle = new Rectangle(10, 20);
+const square = new Square(10);
+console.log(calculateArea(rectangle));
+console.log(calculateArea(square));
+
+
+//Example three
+
+//Payment Processor
+//Credit Card
+//Debit Card
+//PayPal
+
+
+abstract class PaymentProcessor{
+	abstract processPayment(amount:number): void;
+}
+
+class CreditCardProcessor extends PaymentProcessor{
+	processPayment(amount: number): void{
+		console.log(`Paying through Credit Card: ${amount}`);
+	}
+
+}
+
+class PayPal extends PaymentProcessor{
+	processPayment(amount: number): void{
+		console.log(`Paying through PayPal: ${amount}`);
+	}
+}
+
+
+class DebitCardProcessor extends PaymentProcessor{
+	processPayment(amount: number): void{
+		console.log(`Paying through Debit Card: ${amount}`);
+	}
+}
+
+//Client code
+
+function executePayment(paymentProcessor: PaymentProcessor, amount: number):void{
+	paymentProcessor.processPayment(amount);
+}
+
+const creditCardProcessor = new CreditCardProcessor();
+const payPal = new PayPal();
+const debitCardProcessor = new DebitCardProcessor();
+console.log(executePayment(creditCardProcessor, 100));
+console.log(executePayment(payPal, 200));
+console.log(executePayment(debitCardProcessor, 300));
